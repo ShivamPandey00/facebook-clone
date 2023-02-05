6f4fb1299611ba4aeccb91b917e8faf6b6510e85
shivam <shivampandey870@gmail.com>
2023-02-05 08:40:26 -0800

    facevook

---
+++ .js
@@ -0,0 +1,15 @@
+const form = document.querySelector('#post-form');
+const postsContainer = document.querySelector('#posts');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const text = form.querySelector('#post-text').value;
+  if (!text) return;
+
+  const post = document.createElement('div');
+  post.innerHTML = text;
+  postsContainer.appendChild(post);
+
+  form.reset();
+});
